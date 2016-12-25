70eb73f771ba0d5061b51622d245f5926949a056
Alex Lau <alex.lau@youearnedit.com>
2016-12-25 14:46:29 -0600

    mvp - initial commit

---
+++ visualization.js
@@ -0,0 +1,30 @@
+var drawCircles = function (domSelector) {
+  var svg,
+      innerCircle,
+      outerCircle,
+      isLocked;
+  var defaultWidth = 20;
+  var rectangleSizes = [40, 40, 50, 40];
+
+  svg = d3.select(domSelector)
+    .append('svg')
+    .attr("height", 150)
+    .attr("width", 500)
+    .style("background-color", "red");
+
+
+  outerCircle = svg.selectAll("rect")
+    .data(rectangleSizes)
+    .enter()
+    .append("rect")
+    .attr('class', 'locked-on-circle')
+    .attr('y', function (d) { return 100 - d; })
+    .attr('x', function (d, i) {
+      var margin = defaultWidth;
+      var offset = i * (defaultWidth + margin);
+      return offset + margin;
+    })
+    .attr('width', defaultWidth)
+    .attr('height', function (d) { return d; })
+    .style("fill", "#303030")
+};
